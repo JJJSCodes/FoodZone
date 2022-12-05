3983cdeceeb53e0ab4de939291b743e010aa4507
Hong <hongvo@stanford.edu>
2022-12-05 11:30:05 -0800

    select meal

---
+++ components/selectMeal.js
@@ -0,0 +1,8 @@
+import { Text, View, Image, StyleSheet, ScrollView, SafeAreaView, Pressable, navigation } from 'react-native';
+import Header from './Header';
+
+export default function selectMeal() {
+    return (
+<Text>Select Meal</Text>
+    )
+}
