69afa49bd61593bc4ae43d59decb877f278f0668
Hong <hongvo@stanford.edu>
2022-12-05 11:59:31 -0800

    home screens

---
+++ components/HomeInfo/selectMeal.js
@@ -0,0 +1,9 @@
+import { Text, View, Image, StyleSheet, ScrollView, SafeAreaView, Pressable, navigation } from 'react-native';
+import Header from '../Header';
+
+
+export default function selectMeal() {
+    return (
+<Text>Select Meal</Text>
+    )
+}
