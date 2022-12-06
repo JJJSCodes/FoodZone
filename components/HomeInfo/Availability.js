95dabe44e6172de5c2f28fcbd98e2c83e34e1eb9
Hong <hongvo@stanford.edu>
2022-12-06 15:02:39 -0800

    availability screen

--- components/HomeInfo/Availability.js
+++ components/HomeInfo/Availability.js
@@ -6,71 +6,70 @@ export default function Availability({ navigation, route: { params } }) {
     return (
         <View style={styles.screenContainer}>
             <ScrollView style={styles.scrollView}>
-                <Header heading='Shift View' subHeading='My Shift' />
+                <Header heading='Shift View' subHeading='My Shift' back={() => navigation.goBack()}/>
             </ScrollView>
 
-            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <View style={{ flexDirection: "row", justifyContent: "center", alignItems:"center" }}>
                 <Entypo name="arrow-bold-left" size={24} color="black" />
                 <Text style={{ fontSize: 20, fontWeight: "700", paddingHorizontal: 10 }}> Nov 13 - Nov 20</Text>
                 <Entypo name="arrow-bold-right" size={24} color="black" />
             </View>
 
             <View style={styles.shift}>
-                <View style={{ margin: 10 }}>
-                    <Entypo name="circle" size={24} color="black" /> 
-                </View> 
-                <View>
-                    <Text style={styles.meal}>Eggs and Sausage</Text>
-                    <Text style={styles.time}> Breakfast</Text>
-                </View>
-                
-                <Text style={styles.day}> Nov 14</Text>
-            </View>
-            
-            <View style={styles.shift}>
-                <View style={{ margin: 10 }}>
+                <View style={{ margin: 10, flexDirection: "row", alignItems:"center"}}>
                     <Entypo name="circle" size={24} color="black" />
+                    <View style = {{marginHorizontal: 10}}>
+                        <Text style={styles.meal}>Eggs and Sausage</Text>
+                        <Text style={styles.time}>Breakfast</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.meal}>Three Bean Soup</Text>
-                    <Text style={styles.time}> Lunch</Text>
-                </View>
-                <Text style={styles.day}> Nov 14</Text>
+                <Text style={styles.day}>Nov 14</Text>
             </View>
 
             <View style={styles.shift}>
-                <View style={{ margin: 10 }}>
+                <View style={{ margin: 10, flexDirection: "row", alignItems:"center"}}>
                     <Entypo name="circle" size={24} color="black" />
+                    <View style = {{marginHorizontal: 10}}>
+                        <Text style={styles.meal}>Three Bean Soup</Text>
+                        <Text style={styles.time}>Lunch</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.meal}>Steak and Potatoes</Text>
-                    <Text style={styles.time}> Dinner</Text>
-                </View>
-                <Text style={styles.day}> Nov 16</Text>
+                <Text style={styles.day}>Nov 14</Text>
             </View>
 
             <View style={styles.shift}>
-                <View style={{ margin: 10 }}>
+                <View style={{ margin: 10, flexDirection: "row", alignItems:"center"}}>
                     <Entypo name="circle" size={24} color="black" />
+                    <View style = {{marginHorizontal: 10}}>
+                        <Text style={styles.meal}>Steak and Potatoes</Text>
+                        <Text style={styles.time}>Dinner</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.meal}>Fish and Chips</Text>
-                    <Text style={styles.time}> Lunch</Text>
+                <Text style={styles.day}>Nov 16</Text>
+            </View>
+
+
+            <View style={styles.shift}>
+                <View style={{ margin: 10, flexDirection: "row", alignItems:"center"}}>
+                    <Entypo name="circle" size={24} color="black" />
+                    <View style = {{marginHorizontal: 10}}>
+                        <Text style={styles.meal}>Fish and Chips</Text>
+                        <Text style={styles.time}>Lunch</Text>
+                    </View>
                 </View>
-                <Text style={styles.day}> Nov 17</Text>
+                <Text style={styles.day}>Nov 17</Text>
             </View>
 
             <View style={styles.shift}>
-                <View style={{ margin: 10 }}>
+                <View style={{ margin: 10, flexDirection: "row", alignItems:"center"}}>
                     <Entypo name="circle" size={24} color="black" />
+                    <View style = {{marginHorizontal: 10}}>
+                        <Text style={styles.meal}>Ham and Cheese Sandwich</Text>
+                        <Text style={styles.time}>Breakfast</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.meal}>Ham and Cheese Sandwich</Text>
-                    <Text style={styles.time}> Breakfast</Text>
-                </View>
-                <Text style={styles.day}> Nov 19</Text>
+                <Text style={styles.day}>Nov 19</Text>
             </View>
-
         </View>
     )
 }
@@ -99,8 +98,7 @@ const styles = StyleSheet.create({
     day: {
         color: "grey",
         fontSize: 17,
-        justifyContent: "flex-end",
-        alignSelf: "flex-end",
+        alignSelf: "center",
         marginHorizontal: 30
     }
 })
